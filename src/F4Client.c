1d6493ebdc9bde79299440fda7182267d59b3185
maftahoualid <maftahoualid@gmail.com>
2023-05-29 21:13:50 +0200

    aggiunti commenti

--- src/F4Client.c
+++ src/F4Client.c
@@ -7,42 +7,64 @@
 
 int main(){
 
-    int shm_key;
-    int shm_id;
-    int *shm_ptr;
 
-    int sem_key;
-    int sem_id;
-    int sem_num = 10; 
+    int shm_key; // CHIAVE MEMORIA CONDIVISA //
+    int *shm_ptr; // PUNTATORE A MEMORIA CONDIVISA //
+    int shm_id; // ID MEMORIA CONDIVISA //
 
-    int msq_key;
-    int msq_id;
+
+    int sem_key; // CHIAVE SET SEMAFORI //
+    int sem_id; // ID SET SEMAFORI //
+    int sem_num = 10; // NUMERO SEMAFORI //
+
+
+    int msq_key; // CHIAVE CODA MESSAGGI //
+    int msq_id; // ID CODA MESSAGGI //
+
 
     int rows, cols;
 
+
+    // CREO UNA CHIAVE PER IL SET DI SEMAFORI //
     sem_key = ftok("../.",'b');
     printf("[CLIENT DEBUG] shm_key: %d\n",sem_key);
+    // CREO IL SET DI SEMAFORI //
     sem_id = get_sem(sem_key,sem_num);
     printf("[CLIENT DEBUG] shm_id: %d\n",sem_id);
 
+
+    // SBLOCCO IL SERVER [SIGNAL 0] //
     signal_sem(sem_id, 0);
+    // CLIENT CREATO //
     printf("[CLIENT DEBUG]: Client creato\n");
 
+
+    // MI BLOCCO PER ASPETTARE IL MESSAGGIO //
     wait_sem(sem_id, 1);
+    // IL SERVER MI HA SBLOCCATO //
+
+
+    // CREO LA STRUTTURA PER SALVARE IL MESSAGGIO //
     struct matrix_dim dim1;
+    // MI COLLEGO ALLA CODA DI MESSAGGI //
     msq_key = ftok("../.", 'c');
     msq_id = msgget(msq_key, IPC_CREAT | 0666 );
+    // RICEVO IL MESSAGGIO INVIATO DAL SERVER //
+    // [!!!] una volta letto un messaggio, questo viene tolto dalla coda
     msgrcv(msq_id,&dim1,sizeof(int)*2,(long)1.0,0);
-    printf("[%d,%d]",dim1.rows,dim1.cols);
+    // STAMPO I CAMPI RIGA E COLONNA DEL MESSAGGIO RICEVUTO
+    printf("[CLIENT DEBUG] [%d,%d]",dim1.rows,dim1.cols);
 
+
+    // MI COLLEGO ALLA MEMORIA CONDIVISA 
     shm_key = ftok("../.",'a');
     shm_id = get_shm(shm_key, sizeof(int[dim1.rows][dim1.cols]));
+    // LA MAPPO SUL PROCESSO CLIENT //
     shm_ptr = at_shm(shm_id, 0);
 
-    /*while (1){
-        wait_sem(sem_id);
-        print_matrix(shm_ptr,dim.rows,dim.cols);
-    }*/
+
+    // FACCIO IL DETACH DELLA MEMORIA CONDIVISA // 
+    dt_shm(shm_id);
     
     return 0;
 }
--- src/F4Server.c
+++ src/F4Server.c
@@ -8,27 +8,33 @@
 
 int main(int argc, char **argv) {
 
+
     /* CONTROLLO VALIDITA' DEI PARAMETRI */
     check_args(argc, argv);
-    
     /* DO UN NOME AI PARAMETRI */
     int rows = atoi(argv[1]);
     int cols = atoi(argv[2]);
 
-    signal(SIGINT, handle_sigint);
-    signal(SIGALRM, handle_sigalrm);
+    // GESTISCO I SEGNALI
+    signal(SIGINT, handle_sigint); //GESTISCO LA PRESSIONE DEL CTRL-C
+    signal(SIGALRM, handle_sigalrm); //GESTISCO IL TIMER PER IL SECONDO CTRL-C
 
-    int shm_key; 
-    int *shm_ptr; // PUNTATORE A MEMORIA CONDIVISA
-    int shm_id; // ID MEMORIA CONDIVISA
 
-    int sem_key;
-    int sem_id; // ID SEMAFORI
-    int sem_num = 10; // NUMERO SEMAFORI
+    int shm_key; // CHIAVE MEMORIA CONDIVISA //
+    int *shm_ptr; // PUNTATORE A MEMORIA CONDIVISA //
+    int shm_id; // ID MEMORIA CONDIVISA //
+
+
+    int sem_key; // CHIAVE SET SEMAFORI //
+    int sem_id; // ID SET SEMAFORI //
+    int sem_num = 10; // NUMERO SEMAFORI //
     
-    int msq_key;
-    int msq_id;
 
+    int msq_key; // CHIAVE CODA MESSAGGI //
+    int msq_id; // ID CODA MESSAGGI //
+
+
+    // CREO UNA CHIAVE PER LA MEMORIA CONDIVISA //
     shm_key = ftok("../.",'a');
     printf("[SERVER DEBUG] shm_key: %d\n",shm_key);
     // CREO LA MEMORIA CONDIVISA //
@@ -37,12 +43,15 @@ int main(int argc, char **argv) {
     // FACCIO L'ATTACH DELLA MEMORIA //
     shm_ptr = at_shm(shm_id, 0);
     printf("[SERVER DEBUG] shm_ptr: %p\n", shm_ptr);
+
+
     // INIZIALIZZO LA MATRICE //
     init_matrix(shm_ptr,rows,cols);
-
     // STAMPO LA MATRICE // 
     print_matrix(shm_ptr,rows,cols);
 
+
+    // CREO UNA CHIAVE PER IL SET DI SEMAFORI //
     sem_key = ftok("../.",'b');
     printf("[SERVER DEBUG] sem_key: %d\n", sem_key);
     // CREO IL SET DI SEMAFORI //
@@ -51,25 +60,57 @@ int main(int argc, char **argv) {
     // INIZIALIZZO IL SET DI SEMAFORI //
     ctl_sem(sem_id,sem_num);
 
-    printf("[SERVER DEBUG] In attesa che i due client si colleghino.\n");
-    wait_sem(sem_id, 0);
-    printf("[SERVER DEBUG] Client 1 collegato\n");
-    wait_sem(sem_id, 0);
-    printf("[SERVER DEBUG] Client 2 collegato\n");
 
+    
+    
+    // ASPETTO IL PRIMO CLIENT // 
+    printf("[SERVER DEBUG] In attesa che i due client si colleghino.\n");
+    // MI BLOCCO PER ASPETTARE IL PRIMO CLIENT [WAIT 0] //
+    wait_sem(sem_id, 0);
+    // IL PRIMO CLIENT MI HA SBLOCCATO [SIGNAL 0] //
+    printf("[SERVER DEBUG] Client 1 collegato\n");
+    
+
+    // CREO IL MESSAGGIO // 
     struct matrix_dim dim1 = {(long)1.0, rows, cols};
+    // CREO LA CODA DI MESSAGGI //
     msq_key = ftok("../.", 'c');
     msq_id = msgget(msq_key, IPC_CREAT | 0666 );
+    // INVIO IL MESSAGGIO //
     msgsnd(msq_id,&dim1,sizeof(int)*2,0);
+    
+    
+    // SBLOCCO IL PRIMO CLIENT [SIGNAL 1] //
     signal_sem(sem_id, 1);
 
+    
+    // ASPETTO IL SECONDO CLIENT //
+    wait_sem(sem_id, 0);
+    // IL SECONDO CLIENT MI HA SBLOCCATO //
+    printf("[SERVER DEBUG] Client 2 collegato\n");
+
+    
+    // CREO IL MESSAGGIO
+    struct matrix_dim dim2 = {(long)1.0, rows, cols};
+    // INVIO IL MESSAGGIO //
+    msgsnd(msq_id,&dim2,sizeof(int)*2,0);
+
+
+    // SBLOCCO IL SECONDO CLIENT //
+    signal_sem(sem_id, 1);
+
+
+    // GESTIONE DEL GIOCO E DELLA VITTORIA //
     forza4(sem_id,shm_ptr,rows,cols);
+
 
     // FACCIO IL DETACH DELLA MEMORIA // 
     dt_shm(shm_ptr);
 
+
     // RIMUOVO LA MEMORIA CONDIVISA // 
     rm_shm(shm_id);
+
 
     return 0;
 }
