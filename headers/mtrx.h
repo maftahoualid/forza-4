32df50245647a82ce87511881ca7f627b3de9393
maftahoualid <maftahoualid@gmail.com>
2023-06-04 00:45:35 +0200

    aggiunta struct matrix

--- headers/mtrx.h
+++ headers/mtrx.h
@@ -3,7 +3,12 @@
 
 #include <stdio.h>
 
-
+struct matrix {
+    int rows;
+    int cols;
+    char sym1;
+    char sym2;
+};
 
 void init_matrix(void *shm_ptr, int rows, int cols);
 void print_matrix(void *shm_ptr, int rows, int cols);
